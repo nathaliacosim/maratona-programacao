a245e08791f87562d873e73ee5ae7ad07f81d2f0
nathaliacosim <nathalia.cosim@novaandradina.org>
2018-06-08 17:17:08 -0400

    Exercicios Roteiro 1

--- Roteiro-1/Python/Quadrado/Quadrado-Nathalia.py
+++ Roteiro-1/Python/Quadrado/Quadrado-Nathalia.py
@@ -1,5 +1,3 @@
 n = int(input())
-
-if(n <= 10000):
-    resultado = n*n
-    print(resultado)
+resultado = n*n
+print(resultado)
---
+++ Roteiro-1/Python/Soma/Soma-Nathalia.py
@@ -0,0 +1,8 @@
+num = []
+n = int(input())
+for lista in range(n):
+    y = int(input())
+    num.append(int(y))
+    
+soma = sum(num)
+print(soma)
